4f4203cd495f3560702340ff0f497bee4ea611b8
Anthony Leon <Acidknights89@gmail.com>
2020-12-28 17:17:43 -0500

    Added attributes to daily task model.

--- app/models/daily_task_entry.rb
+++ app/models/daily_task_entry.rb
@@ -1,2 +1,5 @@
 class DailyTaskEntry < ActiveRecord::Base
+
+    belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
    has_secure_password
+
+   has_many :daily_task_entries
 end
