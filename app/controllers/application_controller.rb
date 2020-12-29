bb92efefabba3579a5ef120f4f70bff94ec8ba7a
Anthony Leon <Acidknights89@gmail.com>
2020-12-29 02:58:39 -0500

    Enabled sessions in ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set sessions_secret, "daily_task_tracker_app"
   end
 
   get "/" do
