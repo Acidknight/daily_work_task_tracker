b03839e898d74e9d0225f8e2120ee96bbab75fa9
Anthony Leon <Acidknights89@gmail.com>
2020-12-31 01:39:57 -0500

    Added the ability to create a task with 5 attributes for users.

--- app/controllers/daily_tasks_controller.rb
+++ app/controllers/daily_tasks_controller.rb
@@ -2,8 +2,24 @@ class DailyTasksController < ApplicationController
 
     get '/daily_task_entries/new' do
         erb :'/daily_task_entries/new'
+    end
+
+    post '/daily_task_entries' do 
+        if !logged_in?
+            redirect '/'
+        end
+        if params[:title] && params[:date] && params[:description] && params[:time] && params[:notes] != ""
+            @daily_task_entries = DailyTaskEntry.create(params)
+            @user_id = current_user.id
+            redirect "/daily_task_entries/#{@daily_task_entries.id}"
+        else
+            redirect '/daily_task_entries/new'
+        end
+
+            
 
     end
+
 
 
 end
