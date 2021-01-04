5cc753ec03e0a057ec9d71c1b075bdfa63a161e8
Anthony Leon <Acidknights89@gmail.com>
2021-01-03 20:20:26 -0500

    Added Sinatra flash gem.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "daily_task_tracker_app"
+    register Sinatra::flash
   end
 
   get "/" do
