a5eecc4b6f57cd256d9ba6b2efbe319f98f2e1d6
Anthony Leon <Acidknights89@gmail.com>
2021-01-03 20:59:12 -0500

    Fixing spelling error in gem name.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "daily_task_tracker_app"
-    register Sinatra::flash
+    register Sinatra::Flash
   end
 
   get "/" do
